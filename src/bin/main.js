03b2610364c94a042ed5616b438dc7b7aa3d45b4
jonathand alberto serrano serrano <Jonathand@Jonathand-PC.(none)>
2012-04-09 22:52:50 -0500

    Se mejoro el sistema de logs de acuerdo a los parametros de configuracion logger.

--- src/bin/main.js
+++ src/bin/main.js
@@ -69,78 +69,81 @@ function Main(){
 	}
 	this.runPlugins=function(){
 		this.insertLog('runPlugins function called!','info');
-		console.log('una vez mas');
 		if(this.plugins.length>0){
 			this.insertLog('Plugins found!','debug');
 			var index=this.indexPlugin;
 			var state=this.plugins[this.indexPlugin].state;
+			console.log('indice--->		'+index);
+			console.log('estado--->		'+state);
+			console.log('tamano--->		'+this.plugins.length);
 			this.insertLog('Current plugin: '+this.plugins[this.indexPlugin].name+', state: '+state,'debug');
-			console.log('estado-->'+state+' estado -->'+index);
 			if(index<this.plugins.length){ 	
 				switch(state){
 					case 'stop':
 						this.insertLog('plugin :'+this.plugins[index].name+'...started','debug');
 						this.plugins[this.indexPlugin].state='start';
 						this.loadPlugin();
-						console.log('caso stop');
+						console.log('caso--->		stop');
 						break;
 					case 'start':
-						console.log('caso start');
+						console.log('caso--->		start');
 						break;
 					case 'finish':
 						index++;
 						this.indexPlugin=index;
-						iniciarPlugin();
-						console.log('caso finish');
+						this.loadPlugin();
+						console.log('caso-->		finish');
 						break;	
 				}
-				//setTimeout("this.runPlugins()",this.timeOut);
 			}
 		}else this.insertLog("Plugins no found",'debug');
-			
-		
-		
 	}
 	this.loadPlugin=function(){
 		this.insertLog('loadPlugin function called!','info');
 		var pluginPath=this.plugins[this.indexPlugin].pluginPath;
-		console.log('plugin '+pluginPath);
+		console.log('pluginPath--->		'+pluginPath);
 		this.requireFile(pluginPath);
 		this.requireFile(this.currentPath+'/bin/PublicAPI.js');
-		console.log(this.currentPath+'/bin/PublicAPI.js');
 		eval("var Plugin="+this.plugins[this.indexPlugin].name+";");
-		console.log(this.plugins[this.indexPlugin].state);
+		console.log('estado--->		'+this.plugins[this.indexPlugin].state);
 		var newPlugin= new Plugin();
 		newPlugin.api=new PublicAPI(this.plugins[this.indexPlugin]);
 		newPlugin.run();
-		console.log('estado dentro de loadPlugin--->'+this.plugins[this.indexPlugin].state);
-		setTimeout("this.runPlugins()",this.timeOut);
-		//this.plugins[this.indexPlugin].state=newObject.api.state;
-		//setTimeout("this.ejecutar_plugins",this.timeOut);
-		//console.log(this.plugins[this.indexPlugin].state);
-		//console.log(objConfigPlugin.estado);
-		//this.pluginEvaluator(objConfigPlugin);
-			
-		
-		/*if(this.validar_directorio(this.pluginsPath)){
-			if(this.obtener_plugins()){
-				this.ejecutar_plugins(config);
-			}
-		}*/
-		//setTimeout("this");
-		
+		console.log('estadoDesdeLoad--->		'+this.plugins[this.indexPlugin].state);
+		//var ini=this.runPlugins();
+		//var _self=this;
+		setTimeout(this.runPlugins,this.timeOut);
 	}
 	this.insertLog=function(msg,level){
-		var timestamp=new Date();
-		var logPath=this.currentPath+"/../logs/sysLog.log";
-		if(!fs.exists(logPath)){
-			fs.touch(logPath);
+		if(typeof this.logLevels=='undefined'){
+			level='debug';
+			var timestamp=new Date();
+			var logPath=this.currentPath+"/../logs/sysLog.log";
+			if(!fs.exists(logPath)){
+				fs.touch(logPath);
+			}
+			var file=fs.open(logPath,'a');
+			var log=level+":	"+timestamp+" "+msg;
+			file.writeLine(log); 
+			file.close();
+		}else{
+			level = level && this.logLevels.indexOf(level) > -1 ? level : "debug";
+			var ind=this.logLevels.indexOf(level);
+			if (this.logLevels.indexOf(level) < this.logLevels.indexOf(this.logger.level)) {
+				// skip ;)
+			}
+			else{
+				var timestamp=new Date();
+				var logPath=this.currentPath+"/../logs/sysLog.log";
+				if(!fs.exists(logPath)){
+					fs.touch(logPath);
+				}
+				var file=fs.open(logPath,'a');
+				var log=level+":	"+timestamp+" "+msg;
+				file.writeLine(log); 
+				file.close();
+			}
 		}
-		var file=fs.open(logPath,'a');
-		var log=level+":	"+timestamp+" "+msg;
-		file.writeLine(log); 
-		file.close();
-		
 	}
 	this.readConfig=function(){
 		this.insertLog('readConfig function called!','info');
@@ -151,6 +154,11 @@ function Main(){
 		this.insertLog(this.config_path+' Loading configuration parameters ...','info');
 		this.pluginsPath=config.paths.pluginsPath;
 		this.insertLog('Plugins path loaded!','info');
+		this.logger=config.loggger;
+		this.insertLog('Logger options loaded!','info');
+		this.logLevels=new Array("debug","info","warning","error");
+		//this.logLevels={'0':"debug",'1':"info",'2':"warning",'3':"error"};
+		this.insertLog('Log levels loaded!','info');
 		this.includesPath=config.paths.includesPath;
 		this.insertLog('Includes path loaded!','info');
 		this.casperPath=config.paths.casperPath;
@@ -159,8 +167,6 @@ function Main(){
 		this.insertLog('Timeout parameter loaded!','info');
 		this.insertLog('Configuration parameters loaded!','info');
 	}
-	
-	
 	this.pluginExit=function(){//exit the plataforma.js
 		phantom.exit();
 	}
